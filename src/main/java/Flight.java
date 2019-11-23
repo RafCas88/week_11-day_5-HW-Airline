76dcff3b39cfdd9e560771410e37b3347939246b
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-11-23 12:08:38 +0000

    flight starts empty tested

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,34 +1,37 @@
 import java.util.ArrayList;
 
 public class Flight {
+
     private ArrayList<Passenger> passengers;
-    private Plane plane;
     private String flightNumber;
     private String destination;
     private String departureAirport;
     private String departureTime;
 
     public Flight(
-            Plane plane,
+            ArrayList<Passenger> passengers,
             String flightNumber,
             String destination,
             String departureAirport,
             String departureTime){
-        this.passengers = new ArrayList<Passenger>();
-        this.plane = plane;
+        this.passengers = passengers;
         this.flightNumber = flightNumber;
         this.destination = destination;
         this.departureAirport = departureAirport;
         this.departureTime = departureTime;
     }
 
-    public int getNumberOfAvailableSeats(){
-        ArrayList<Passenger> availableSeats = new ArrayList<Passenger>();
-        for (Passenger passenger :this.passengers){
-            if(passenger.passengerCount() == 0){
-                availableSeats.add(passenger);
-            }
-        }
-        return availableSeats;
+    public int passengerCount() {
+        return this.passengers.size();
     }
+
+//    public int getNumberOfAvailableSeats(){
+//        ArrayList<Passenger> availableSeats = new ArrayList<Passenger>();
+//        for (Passenger passenger :this.passengers){
+//            if(passenger.passengerCount() == 0){
+//                availableSeats.add(passenger);
+//            }
+//        }
+//        return availableSeats;
+//    }
 }
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightTest {
+
+    private Flight flight;
+    private Passenger passenger;
+    private Plane plane;
+
+    @Before
+    public void before(){
+
+        ArrayList<Passenger> passengers = new ArrayList<Passenger>();
+
+        flight = new Flight(
+                passengers,
+                "FR756",
+                "GLA",
+                "EDI",
+                "10:00");
+
+        plane = new Plane(PlaneType.BOEING747);
+    }
+
+    @Test
+    public void flightStartsEmpty(){
+        assertEquals(0, flight.passengerCount());
+    }
+
+}
